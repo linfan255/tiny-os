d2ca739d016d476f8e0d9c5f9f6b3b71e47e683a
林范 <linfan255@163.com>
2017-07-20 17:14:16 +0800

    Update init.c

--- kernel/init.c
+++ kernel/init.c
@@ -1,9 +1,11 @@
 #include "init.h"
 #include "print.h"
 #include "interrupt.h"
+#include "timer.h"
 
 void init()
 {
 	put_str("init all\n");
-	init_idt();
+	init_idt();		//初始化中断
+	init_8253();	//初始化8253计数器，提高时钟中断发生频率
 }
